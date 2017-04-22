491237b4b32b1528deb080074a38cd63f65331f9
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-04-22 17:47:00 +0200

    House Prefabs

---
+++ Assets/Prefabs/GameManager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private float currentScore = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void AddPoints(float additionalPoints)
+    {
+        currentScore += additionalPoints;
+    }
+    public float GetCurrentPoints()
+    {
+        return currentScore;
+    }
+
+}
---
+++ Assets/Prefabs/ScoreTracker/ScoreTracker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreTracker : MonoBehaviour {
+
+    public Camera cam;
+
+    private Text textField;
+    private string currentScore = "0";
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        textField = GetComponent<Text>();
+        gameManager = FindObjectOfType<GameManager>().GetComponent<GameManager>();
+    }
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        if (cam != null)
+        {
+            // Tie this to the camera, and do not keep the local orientation.
+            transform.SetParent(cam.GetComponent<Transform>(), true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        currentScore = gameManager.GetComponent<GameManager>().GetCurrentPoints().ToString();
+        textField.text = "Score: " + currentScore;
+	}
+}
