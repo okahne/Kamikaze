5f52a841a57aab17dbd240989083a21f44f66f73
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2017-04-23 14:20:04 +0200

    Added cartoon style to scene

---
+++ Assets/Prefabs/DestroyAfterTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTimer : MonoBehaviour {
+
+    [SerializeField]
+    float timeUntilDestruction = 1f;
+
+    private float lifeTime;
+	// Use this for initialization
+	void Start () {
+        lifeTime = 0f; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime = lifeTime + Time.deltaTime;
+        if (lifeTime>timeUntilDestruction)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Prefabs/Ignite.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Ignite : MonoBehaviour {
+
+    public AudioClip explosionSound;
+    public GameObject explosion;
+
+    private AudioSource audioSource;
+    private string buildingTag = "Building";
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(buildingTag))
+        {
+            other.gameObject.GetComponent<Building>().SetOnFire();
+            Instantiate(explosion, transform.position,transform.rotation);
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
